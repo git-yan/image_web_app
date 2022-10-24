90bb23fd351de3a122338b4f9b3bac4d28f02918
git-yan <68041169+git-yan@users.noreply.github.com>
2022-10-24 18:11:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,27 +2,27 @@ import streamlit as st
 from PIL import Image
 from clf import predict
 import time
-
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
 st.title("VisualFeast Simple Image Classification App")
 st.write("")
 st.write("")
-option = st.selectbox('Choose the model you want to use?',
-                      ('resnet50', 'resnet101', 'densenet121', 'shufflenet_v2_x0_5', 'mobilenet_v2'))
+option = st.selectbox(
+     'Choose the model you want to use?',
+     ('resnet50', 'resnet101', 'densenet121','shufflenet_v2_x0_5','mobilenet_v2'))
 ""
 option2 = st.selectbox(
-    'you can select some image',
-    ('image_dog', 'image_snake'))
+     'you can select some image',
+     ('image_dog', 'image_snake'))
 
 file_up = st.file_uploader("Upload an image", type="jpg")
 if file_up is None:
-    if option2 == "image_dog":
-        image = Image.open("./image/dog.jpg")
-        file_up = "./image/dog.jpg"
+    if option2 =="image_dog":
+        image=Image.open("image/dog.jpg")
+        file_up="image/dog.jpg"
     else:
-        image = Image.open("./image/snake.jpg")
-        file_up = "./image/snake.jpg"
+        image=Image.open("image/snake.jpg")
+        file_up="image/snake.jpg"
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
     st.write("Just a second...")
@@ -43,7 +43,7 @@ else:
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
     st.write("Just a second...")
-    labels, fps = predict(file_up, option)
+    labels,fps = predict(file_up,option)
 
     # print out the top 5 prediction labels with scores
     st.success('successful prediction')
@@ -53,4 +53,4 @@ else:
     # print(t2-t1)
     # st.write(float(t2-t1))
     st.write("")
-    st.metric("", "FPS:   " + str(fps))
+    st.metric("","FPS:   "+str(fps))
